8124a994ee7c572feef965792b4813c900bc5900
GTFTT <FTowerT@outlook.com>
2026-05-03 17:21:06 +0300

    Added first menu appearing animation

---
+++ src/compoentns/Header/ButtonsContainer/ButtonsContainer.tsx
@@ -0,0 +1,58 @@
+import styles from "./ButtonsContainer.module.css";
+import {useEffect, useRef} from "react";
+import {animate, createScope, createTimeline, type Scope} from "animejs";
+
+export interface ButtonsContainerProps {
+  className?: string;
+  isExpanded: boolean;
+}
+
+function ButtonsContainer(props: ButtonsContainerProps) {
+  const rootRef = useRef<HTMLDivElement | null>(null);
+  const scopeRef = useRef<Scope | null>(null);
+
+  useEffect(() => {
+      if(!rootRef.current) {
+        throw new Error(`Root reference is not available yet.`);
+      }
+      scopeRef.current = createScope({ root: rootRef.current });
+      scopeRef.current.add(self => {
+        if(self === undefined) throw new Error(`Scope is not available yet.`);
+        self.add('appear', () => {
+          if(!rootRef.current) throw new Error(`Root reference is not available yet.`);
+          const tm = createTimeline()
+          tm.add(rootRef.current, {
+            opacity: 1,
+            minHeight: '50vh',
+            duration: 1000,
+            ease: 'inQuad',
+          })
+        })
+        self.add('disappear', () => {
+          if(!rootRef.current) throw new Error(`Root reference is not available yet.`);
+          animate(rootRef.current, {
+            opacity: 0,
+            minHeight: '0vh',
+            duration: 1000,
+            ease: 'inQuad',
+          })
+        })
+      })
+  }, []);
+
+  useEffect(() => {
+    if (props.isExpanded) {
+      scopeRef.current?.methods.appear();
+    } else {
+      scopeRef.current?.methods.disappear();
+    }
+  }, [props.isExpanded]);
+
+  return (
+    <div ref={rootRef} className={`${styles.buttonsContainer} ${props.className || ''}`}>
+      CONTENT
+    </div>
+  );
+}
+
+export default ButtonsContainer;
--- src/compoentns/Header/Header.tsx
+++ src/compoentns/Header/Header.tsx
@@ -1,16 +1,86 @@
 import styles from './Header.module.css';
 import MenuActivateButton from "../buttons/MenuActivateButton/MenuActivateButton.tsx";
 import ButtonsContainer from "./ButtonsContainer/ButtonsContainer.tsx";
+import {animate, createScope, Scope} from 'animejs'
+import {useEffect, useRef, useState} from "react";
 
 type Props = {
   className?: string
 };
 
 export const Header = (props: Props) => {
+  const [headerActivated, setHeaderActivated] = useState<boolean>(false);
+  const container = useRef(null);
+  const scope = useRef<Scope>(null);
+  const menuActivateButtonContainerRef = useRef(null);
+
+  useEffect(() => {
+    scope.current = createScope({ root: container });
+    scope.current.add((self) => {
+      if(!self) throw new Error(`Scope is not available yet.`);
+      self.add('activateMenu', () => {
+        if(!menuActivateButtonContainerRef.current) throw new Error(`Menu button reference is not available yet.`);
+        animate(menuActivateButtonContainerRef.current, {
+          opacity: 0,
+          height: '0rem',
+          ease: 'out(1)',
+          duration: 350,
+          delay: 500,
+        });
+      });
+      self.add('deactivateMenu', () => {
+        if(!menuActivateButtonContainerRef.current) throw new Error(`Menu button reference is not available yet.`);
+        animate(menuActivateButtonContainerRef.current, {
+          opacity: 1,
+          height: '2rem',
+          ease: 'out(1)',
+          duration: 200,
+          delay: 1000,
+        });
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if(!scope.current) throw new Error(`Scope is not available yet.`);
+    if(headerActivated) {
+      scope.current.methods.activateMenu()
+    } else {
+      scope.current.methods.deactivateMenu()
+    }
+  }, [headerActivated]);
+
+  const activateMenuClicked = () => {
+    setHeaderActivated(true);
+  }
+
+  const deactivateMenuClicked = () => {
+    setHeaderActivated(false);
+  }
+
   return (
-    <header className={`${styles.header} ${props.className || ''}`}>
-      <MenuActivateButton />
-      <ButtonsContainer isExpanded={true} />
+    <header
+      ref={container}
+      className={`${styles.header} ${props.className || ''}`}
+
+    >
+      <div
+        ref={menuActivateButtonContainerRef}
+        className={styles.activateButtonContainer}
+      >
+        <MenuActivateButton onClick={activateMenuClicked}/>
+      </div>
+      <div className={styles.contentContainer}>
+        <ButtonsContainer isExpanded={headerActivated} />
+        <div
+          className={`${styles.spaceContainer} ${headerActivated? styles.remainingSpaceContainer: styles.zeroSpaceContainer}`}
+          onClick={() => {
+            deactivateMenuClicked()
+          }}
+        >
+          <span>Click anywhere to exit menu</span>
+        </div>
+      </div>
     </header>
   );
 };
