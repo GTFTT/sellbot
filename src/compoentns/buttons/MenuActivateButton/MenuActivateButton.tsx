57ba330210ba79e87dd8dcb7c0d1fff8b9ac920d
GTFTT <FTowerT@outlook.com>
2026-05-09 14:23:45 +0300

    Improved responsive styles for buttons and menu components

--- src/compoentns/buttons/MenuActivateButton/MenuActivateButton.tsx
+++ src/compoentns/buttons/MenuActivateButton/MenuActivateButton.tsx
@@ -1,6 +1,5 @@
 import {type ButtonHTMLAttributes} from 'react';
 import styles from "./MenuActivateButton.module.css";
-import logo from "../../../assets/logo.png"
 
 function MenuActivateButton(props: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
@@ -8,7 +7,6 @@ function MenuActivateButton(props: ButtonHTMLAttributes<HTMLButtonElement>) {
       {...props}
       className={`${styles.menuActivateButton} ${props.className || ''}`}
     >
-      <span className={styles.logoContainer}><img className={styles.logo} src={logo} alt={"logo"}/></span>
       <span>MENU</span>
     </button>
   );
