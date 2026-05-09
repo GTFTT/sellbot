93d48caaeb03cbd0b2753ed60dbdf69d8591eca4
GTFTT <FTowerT@outlook.com>
2026-05-09 12:36:56 +0300

    Fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/GTFTT/',
+  base: '/sellbot/',
   plugins: [react()],
   server: {
     host: 'localhost',
