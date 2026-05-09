3df4a1a5d37fb25a81b0ef6f3987a3e285e9a8b8
GTFTT <FTowerT@outlook.com>
2026-05-09 10:11:10 +0300

    Footer improved

--- src/compoentns/Footer/Footer.tsx
+++ src/compoentns/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css"
 function Footer() {
   return (
     <div className={styles.container}>
-      All rights are protected {(new Date().getFullYear())}
+      All rights protected. We keep this world productive and safe for more than 20 years now 2004-{(new Date().getFullYear())}.
     </div>
   );
 }
